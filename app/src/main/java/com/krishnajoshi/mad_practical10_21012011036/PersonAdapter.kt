24a0e2c84c6a16560a3b35e40978d0b473fa0c3c
Krishna7249 <98690735+Krishna7249@users.noreply.github.com>
2023-10-28 11:09:56 +0530

    pr

--- app/src/main/java/com/krishnajoshi/mad_practical10_21012011036/PersonAdapter.kt
+++ app/src/main/java/com/krishnajoshi/mad_practical10_21012011036/PersonAdapter.kt
@@ -17,8 +17,8 @@ class PersonAdapter(context:Context, val Personarray: ArrayList<Person>):ArrayAd
         view.findViewById<TextView>(R.id.number_textView).text=Personarray[position].phoneNo
         view.findViewById<TextView>(R.id.email_textView).text=Personarray[position].emailId
         view.findViewById<TextView>(R.id.address_textView).text=Personarray[position].address
-        view.findViewById<FloatingActionButton>(R.id.location_btn).setOnClickListener {
-            Intent(context,MapsActivity::class.java).putExtra("object",Personarray[position]).apply {
+        view.findViewById<MaterialButton>(R.id.location_btn).setOnClickListener {
+            Intent(context,MapsActivity::class.java).putExtra("Object",Personarray[position]).apply {
                 context.startActivity(this)
             }
         }
